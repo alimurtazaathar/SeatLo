202cfc8a57dc1d66c9a5cd9e0634eab3f017c487
Syed Ali Murtaza <alimurtazaathar@gmail.com>
2025-05-01 00:28:28 +0500

    Removed client folder from backend branch

--- client/hooks/useLoadFonts.ts
+++
@@ -1,9 +0,0 @@
-import { useFonts } from "expo-font";
-
-export default function useLoadFonts() {
-  const [fontsLoaded] = useFonts({
-    "Vercetti": require("../assets/fonts/Vercetti-Regular.ttf"),
-  });
-
-  return fontsLoaded;
-}
